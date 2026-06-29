7e3968b3fb4021badf5920442b7e8500fe578891
willi <willi@trilogy.com>
2026-06-28 19:17:05 -0500

    fix: update lazy import in login endpoint to use src.infra.authentication

--- jarvis_api.py
+++ jarvis_api.py
@@ -659,7 +659,7 @@ def login(
     if not user or not verify_password(form_data.password, str(user.hashed_password)):
         raise HTTPException(
             status_code=401, detail="Incorrect username or password")
-    from authentication import create_access_token
+    from src.infra.authentication import create_access_token
 
     access_token = create_access_token(
         data={"sub": user.username},
